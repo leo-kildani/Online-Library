24a66159f9dcc169af1f572db4bbcac7950afa80
leo-kildani <leonardo.kildani@sjsu.edu>
2024-05-02 11:04:33 -0700

    implement some entities

---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,53 @@
+package com.example.demo.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+public @Data class User {
+
+    @Id
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "active")
+    private boolean active;
+
+    @Column(name = "dob")
+    private LocalDate dob;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public User(String username, String password, String email, String firstName, String lastName, boolean active, LocalDate dob) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.active = active;
+        this.dob = dob;
+    }
+}
