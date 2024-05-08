c396849b2dbacdb59320b58e59f20ced68843ed7
leo-kildani <leonardo.kildani@sjsu.edu>
2024-05-08 11:10:48 -0700

    fix recommendation method

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -4,7 +4,6 @@ import java.io.UnsupportedEncodingException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.demo.entity.User;
